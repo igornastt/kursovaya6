a267703eb798f1815f0b356ded698941049d237d
igornast <grezle00@gmail.com>
2023-09-16 08:39:50 +0300

    Создание и настройка приложения clients

---
+++ clients/views.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.http import Http404
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
+from django.urls import reverse
+from clients.models import Client
+from clients.forms import ClientForm
+
+
+class ClientCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Client
+    form_class = ClientForm
+    permission_required = 'clients.add_client'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        self.object.owner = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('clients:list')
+
+
+class ClientUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Client
+    form_class = ClientForm
+    permission_required = 'clients.change_client'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.object.owner != self.request.user:
+            raise Http404("Вы не являетесь создателем данного клиента, у вас нет прав на его редактирование.")
+        return self.object
+
+    def get_success_url(self):
+        return reverse('clients:view', args=[self.kwargs.get('pk')])
+
+
+class ClientListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = Client
+    permission_required = 'clients.view_client'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(owner=self.request.user)
+        return queryset
+
+
+class ClientDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    model = Client
+    permission_required = 'clients.view_client'
+
+
+class ClientDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Client
+    permission_required = 'clients.delete_client'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        if self.object.owner != self.request.user:
+            raise Http404("Вы не являетесь создателем данного клиента, у вас нет прав на его удаление.")
+        return self.object
+
+    def get_success_url(self):
+        return reverse('clients:list')
